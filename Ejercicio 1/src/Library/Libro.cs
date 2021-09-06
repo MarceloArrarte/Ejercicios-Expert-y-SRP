253433686382e8c0e67e31066dfddb4fe0b5fb48
Marcelo Arrarte <32116762+MarceloArrarte@users.noreply.github.com>
2021-09-06 18:50:49 -0300

    Crítica al programa basado en modelo SRP

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -8,6 +8,14 @@ namespace SRP
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
+
+        /*
+        Las siguientes propiedades violan aquí el principio de SRP, ya que la responsabilidad
+        de conocer los datos de almacenaje no debería recaer en el libro. Si lo hacen, como
+        en este caso, implica que la clase Libro puede cambiar tanto si se deciden cambiar los
+        datos del libro que se almacenan (agregando por ejemplo ISBN), como si se cambia la
+        estructura de almacenamiento del mismo (agregando por ejemplo número de sala).
+        */
         public string SectorBiblioteca { get ; set; }
         public string EstanteBiblioteca { get ; set; }
 
